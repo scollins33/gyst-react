c89e65f03721ea4afa702b6abe0983be9b952e9c
Michael Daye <mgdaye@gmail.com>
2018-01-18 17:18:39 -0500

    refactored header to center components

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 
 const Header = props => {
-    let headerLogo = <Link to={'/'}><h1 style={{fontFamily: "\"Arial Black\", Gadget, sans-serif"}}>GYST</h1></Link>;
+    let headerLogo = <Link to={'/'}><h1 cl style={{fontFamily: "\"Arial Black\", Gadget, sans-serif"}}>GYST</h1></Link>;
     let headerStyle = {backgroundColor: "#fff", boxShadow: "0px 3px 5px rgba(100, 100, 100, 0.49)"};
     if (props.mainPage === true) {
         headerLogo = null;
@@ -14,10 +14,10 @@ const Header = props => {
             <div
                 className={"container-fluid sticky-top"}
                 style={headerStyle}>
-                <div className={"d-flex p-2 justify-content-between align-items-center"}>
-                    <RouteLinks/>
-                    {headerLogo}
-                    {props.login}
+                <div className={"row"}>
+                    <div className={"col-4 d-flex justify-content-start my-auto"}><RouteLinks/></div>
+                    <div className={"col-4 d-flex justify-content-center my-auto"}>{headerLogo}</div>
+                    <div className={"col-4 d-flex justify-content-end my-auto"}>{props.login}</div>
                 </div>
             </div>
         )
--- client/src/components/RouteLinks.js
+++ client/src/components/RouteLinks.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
 const RouteLinks = props =>(
-    <div className={"d-flex flex-row"}>
-        <p children={"mx-1"}><Link to={"/contact"}>contact us</Link></p>
+    <div className={"d-flex flex-row align-content-start"}>
+        <p className={"mx-1"}><Link to={"/contact"}>contact us</Link></p>
         <p className={"mx-1"}><Link to={'/time'}>time</Link></p>
         <p className={"mx-1"}><Link to={'/money'}>money</Link></p>
         <p className={"mx-1"}><Link to={'/social'}>social</Link></p>
